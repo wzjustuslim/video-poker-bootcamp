857f391bac15b56aed01090eb3da7eda16ead587
wzjustuslim <weizhengjustus.lim@gmail.com>
2021-10-17 14:18:53 +0800

    update for container markups

--- markup.js
+++ markup.js
@@ -5,3 +5,19 @@ document.body.appendChild(generalContainer);
 const gameContainer = document.createElement('div');
 gameContainer.classList.add('game-container');
 generalContainer.appendChild(gameContainer);
+
+const tableContainer = document.createElement('div');
+tableContainer.classList.add('table-container');
+gameContainer.appendChild(tableContainer);
+
+const cardContainer = document.createElement('div');
+cardContainer.classList.add('card-container');
+gameContainer.appendChild(cardContainer);
+
+const infoContainer = document.createElement('div');
+infoContainer.classList.add('info-container');
+gameContainer.appendChild(infoContainer);
+
+const buttonContainer = document.createElement('div');
+buttonContainer.classList.add('button-contaner');
+gameContainer.appendChild(buttonContainer);
